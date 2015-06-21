1b47c12c86f0d7b47083ed0fecd13f76729e1e61
dhw <dhw@reanimated.us>
2015-06-21 17:21:16 -0500

    Changed text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -164,24 +164,24 @@
             <div class="row">
                 <div class="col-md-12 col-sm-12">
                     <h1>What We Do?</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi orci mauris, volutpat portt <br>itor lobortis et, scelerisque ac mauris. Quisque metus enim, congue quis sem ac.</p>
+                    <p>Welcome to the worlds first community service platform. Here at PleasantVille we have made creating and finding community service events as easy as possible.</p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-4 col-sm-4" data-appear-top-offset="-100" data-animated="fadeIn">
                     <img src="{{ asset('images/volunteer.jpg') }}" alt="" />
                     <h2>Volunteer</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi orci </p>
+                    <p>Search through all of the community service opportunities in area and sign up with the click of a button.</p>
                 </div>
                 <div class="col-md-4 col-sm-4" data-appear-top-offset="-200" data-animated="fadeIn">
                     <img src="{{ asset('images/event3.jpg') }}" alt="" />
                     <h2>Care</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi orci </p>
+                    <p>Grow your volunteer base by creating and promoting community service events.</p>
                 </div>
                 <div class="col-md-4 col-sm-4" data-appear-top-offset="-300" data-animated="fadeIn">
                     <img src="{{ asset('images/earn.jpg') }}" alt="" />
                     <h2>Earn</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi orci </p>
+                    <p>Track the progress and completion of community service hours.</p>
                 </div>
             </div>
         </div>
@@ -208,8 +208,6 @@
             <div class="row">
                 <div class="col-md-12 col-sm-12" data-appear-top-offset="-100" data-animated="fadeInUp">
                     <h1>Upcoming Events</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi orci <br>
-                        mauris, volutpat porttitor lobortis et, scelerisque ac mauris. </p>
                     <h2 class="portfolio-heading2"><span class="left_contr">Events Nearby You</span><span class="right_contr">
 <img align="left" alt="arrow" src="{{ asset('images/arrow.jpg') }}">{{ $location["city"] }}</span></h2>
                 </div>
@@ -217,21 +215,21 @@
             <div class="row first">
 
                 @foreach($Events as $event)
-                <div class="mask-area col-md-4 col-sm-4" data-appear-top-offset="-100" data-animated="fadeInLeft">
-                    <a href=""><div class="mask"></div></a>
-                    <img src="{{ asset('images/events/' . $event->id. '.jpg') }}" height="147" width="220" alt="" />
-                    <div class="portfoliotxt">
-                        <h3>{{ $event->name }}</h3>
-                        <p>{{ $event->description }} </p>
-                        <ul>
-                            <li class="sm"><img src="{{ asset('images/tag_icon.jpg') }}" alt="" />Playing children</li>
-                            <li class="sm"><img src="{{ asset('images/user_icon.jpg') }}" /> 126 persons going</li>
-                            <li><img src="{{ asset('images/location_icon.jpg') }}" />{{ $event->address  }}, {{ $event->city }}, {{ $event->state }}</li>
-                            <li><img src="{{ asset('images/calc_cion.jpg') }}" />{{ $event->FriendlyDate($event->start_time) }}</li>
-                            <li><a href="{{ url( $event->organization->slug . '/events/' . $event->slug) }}">Read More</a></li>
-                        </ul>
+                    <div class="mask-area col-md-4 col-sm-4" data-appear-top-offset="-100" data-animated="fadeInLeft">
+                        <a href=""><div class="mask"></div></a>
+                        <img src="{{ asset('images/events/' . $event->id. '.jpg') }}" height="147" width="220" alt="" />
+                        <div class="portfoliotxt">
+                            <h3>{{ $event->name }}</h3>
+                            <p>{{ $event->description }} </p>
+                            <ul>
+                                <li class="sm"><img src="{{ asset('images/tag_icon.jpg') }}" alt="" />Playing children</li>
+                                <li class="sm"><img src="{{ asset('images/user_icon.jpg') }}" /> 126 persons going</li>
+                                <li><img src="{{ asset('images/location_icon.jpg') }}" />{{ $event->address  }}, {{ $event->city }}, {{ $event->state }}</li>
+                                <li><img src="{{ asset('images/calc_cion.jpg') }}" />{{ $event->FriendlyDate($event->start_time) }}</li>
+                                <li><a href="{{ url( $event->organization->slug . '/events/' . $event->slug) }}">Read More</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
                 @endforeach
             </div>
 
@@ -243,19 +241,19 @@
             <div class="row">
                 <div class="col-md-12 col-sm-12">
                     <h1>Find Ways to Help Your Community</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  </p>
+                    <p>Here are a few categories to choose from</p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12 col-sm-12">
                     <div class="serv_links">
-                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot1.png') }}"><br><span>LOREM IPSUM</span></div></a>
-                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot2.png') }}"><br><span>LOREM IPSUM</span></div></a>
-                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot7.png') }}"><br><span>LOREM IPSUM</span></div></a>
-                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot4.png') }}"><br><span>LOREM IPSUM</span></div></a>
-                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot1.png') }}"><br><span>LOREM IPSUM</span></div></a>
-                        <a class="serv rmn rokl" href="#"><div><img src="{{ asset('images/rot6.png') }}"><br><span>LOREM IPSUM</span></div></a>
-                        <a class="serv rmn rokl" href="#"><div><img src="{{ asset('images/rot7.png') }}"><br><span>LOREM IPSUM</span></div></a>
+                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot1.png') }}"><br><span>Animals</span></div></a>
+                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot2.png') }}"><br><span>Soup Kitchen</span></div></a>
+                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot7.png') }}"><br><span>Enviroment</span></div></a>
+                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot4.png') }}"><br><span>Event Setup</span></div></a>
+                        <a class="serv rokl" href="#"><div><img src="{{ asset('images/rot1.png') }}"><br><span>Animals</span></div></a>
+                        <a class="serv rmn rokl" href="#"><div><img src="{{ asset('images/rot6.png') }}"><br><span>Outreach</span></div></a>
+                        <a class="serv rmn rokl" href="#"><div><img src="{{ asset('images/rot7.png') }}"><br><span>Parks & Rec</span></div></a>
                     </div>
                 </div>
             </div>
@@ -266,9 +264,10 @@
             <div class="row">
                 <div class="col-md-12 col-sm-12">
                     <h1>JOIN US TODAY !</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut euismod turpis.
-                        Duis iaculis auctor venenatis. Lorem ipsum dolor sit amet.</p>
-
+                    <p>We see your time as the most precious commodity. Whether you are working at a local soup kitchen or cleaning up a public park, the time that you take out of your life to improve the world around you is truly a wonderful gift. <br />
+                        We’re excited to have you join us. Our goal is to make giving back to your community as easy as possible; exactly how it should be.</p>
+                    <p>Thank you for your time,<br />
+                        The PleasantVille Team</p>
                 </div>
             </div>
             <div class="row">
