e5339c726ffb1334c827d53b0ece33f558bfdeaa
dhw <dhw@reanimated.us>
2015-07-16 12:17:25 -0500

    Fixed email template typo

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -5,7 +5,7 @@
 <body>
 Hello {{ $invite->first_name }},
 
-        Please sign up for PleasantVile using this link <a href="{{ URL::to('/invite/' . $invite->invite_code) }}) }}"> Invite </a>
+        Please sign up for PleasantVile using this link <a href="{{ URL::to('/invite/' . $invite->invite_code) }}"> Invite </a>
 
 </body>
 </html>
