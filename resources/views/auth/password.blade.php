62c7ce89bbcd37d02bf76f9381adf670075e36ae
dhw <dhw@reanimated.us>
2015-07-17 14:02:54 -0500

    Forgot password

---
+++ resources/views/auth/password.blade.php
@@ -0,0 +1,38 @@
+@extends('app')
+@section('title')
+    Forgot Password - Pleasantville.co
+@endsection
+@section('content')
+    <section id="LoginWrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger" style="margin-top:5px">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <div class="LgonBox">
+                        <h2>Forgot Password?</h2>
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                            <ul>
+                                <li><input type="text" class="username" name="email" placeholder="E-Mail Address" value="{{ old('email') }}"/></li>
+                                <li><input type="submit" value="Submit" /></li>
+                            </ul>
+                        </form>
+                        <div class="LgonBoxFotter">
+                            Not a member ? <a href="#">Sign up now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
