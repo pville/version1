f88bf94a6521ea10cd1e7ffb8f841d56991ca2f8
dhw <dhw@reanimated.us>
2015-07-24 20:50:24 -0500

    removed attendance.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -100,7 +100,7 @@ class DashboardController extends Controller {
                     ->join('event', function($join)
                     {
                         $user = Auth::user();
-                       $join->on('attendance.user_id', '=', 6)
+                       $join->on('user_id', '=', $user->id)
                             ->where('event.id', '=', 'attendance.event_id')
                             ->where('attendance.checked_in', '=', true)
                            ->where('event.status', '=', 'ended')
