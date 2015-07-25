84c5341fd487334d8d68d79ec4fa46da866cbf13
dhw <dhw@reanimated.us>
2015-07-24 20:02:31 -0500

    ended

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -103,7 +103,8 @@ class DashboardController extends Controller {
                         $join->on('event.id', '=', 'attendance.event_id')
                             ->where('attendance.user_id', '=', $user->id)
                             ->where('attendance.checked_in', '=', true)
-                            ->where('event.status', '=', 'completed');
+                            ->where('event.status', '=', 'completed')
+                            ->where('event.status', '=', 'ended');
 
                     })
                     ->orderBy('start_time','desc')
