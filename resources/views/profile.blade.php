9b49a2ab0e2298270357fb0f50d3dda0a0a950e3
dhw <dhw@reanimated.us>
2015-09-01 16:55:49 -0500

    added nl2br to profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -29,7 +29,7 @@
                 <div class="col-md-8 col-sm-8">
                     <div class="boxradwrap mission-box" style="padding:15px;">
                         <h1>Our Mission</h1>
-                        <p> {{ $org->description }}</p>
+                        <p>{!! nl2br(e($event->description)) !!}</p>
                      </div>
                 </div>
 
