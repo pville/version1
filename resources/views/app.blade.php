6c3275f4ccbe749369317ee1af3d7bfadba0e149
dhw <dhw@reanimated.us>
2015-09-01 20:28:27 -0500

    Font Awesome

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,6 +15,8 @@
 
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
        
         <link rel="stylesheet" href="{{ asset('css/bootstrap-theme.min.css') }}">
